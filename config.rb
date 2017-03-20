3d65681bfe84f5968537eec1081be189c8f67bc1
ZDonel <zacdonelson@gmail.com>
2017-03-20 18:13:24 -0500

    Different Box image

---
+++ config.rb
@@ -0,0 +1,6 @@
+$box_name   = "bento/ubuntu-16.04"
+$box_memory = 1024
+$box_vcpus  = 2
+
+$ansible_playbook = "bootstrap_env/bootstrap.yml"
+$ansible_inventory = ".vagrant/provisioners/ansible/inventory_override"
